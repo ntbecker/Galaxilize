376df50c926b68412d249f39c6311510f87ee8bd
Nathan Becker <ntbecker@rogers.com>
2024-05-28 13:48:44 -0400

    Application Info

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,10 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("ICS4UFinalProjectGalaxilize");
+		config.setTitle("Galaxilize");
+		config.setWindowedMode(800,800);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
 		new Lwjgl3Application(new Galaxilize(), config);
 	}
 }
