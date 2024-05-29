d0870339c55982509472a225e76de3018f237a3e
SpoonsDev <mawit8903@gmail.com>
2024-05-29 12:54:24 -0400

    Created and commented getters and setters for the Player class.

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -58,4 +58,91 @@ public class Player extends PhysicsObject{
     public void draw(SpriteBatch s){
         s.draw(playerTexture, (float) posX, (float) posY);
     }
+    /**
+     * Returns the current health of the player.
+     * @return the health of the player.
+     */
+    public int getHealth(){ return(health); }
+
+    /**
+     * Sets the health of the player to the input value.
+     * @param health the health of the player.
+     */
+    public void setHealth(int health){ this.health = health; }
+
+    /**
+     * Returns the current fuel of the player.
+     * @return the fuel of the player.
+     */
+    public double getFuel(){ return(fuel); }
+
+    /**
+     * Sets the fuel of the player to the input value.
+     * @param fuel the fuel of the player.
+     */
+    public void setFuel(double fuel){ this.fuel = fuel; }
+
+    /**
+     * Returns the current score of the player.
+     * @return the score of the player.
+     */
+    public int getScore(){ return(score); }
+
+    /**
+     * Sets the score of the player to the input value.
+     * @param score the score of the player.
+     */
+    public void setScore(int score){ this.score = score; }
+
+    /**
+     * Returns if the player is hooked to an asteroid.
+     * @return a boolean holding if the player is hooked to an asteroid.
+     */
+    public boolean getIsHooked(){ return(isHooked); }
+
+    /**
+     * Sets if the player is hooked to an asteroid based on the input value.
+     * @param isHooked a boolena holding if the player is hooked to an asteroid.
+     */
+    public void setIsHooked(boolean isHooked){ this.isHooked = isHooked; }
+
+    /**
+     * Returns the asteroid that the player is hooked onto.
+     * @return the asteroid that the player is hooked onto.
+     */
+    public Asteroid getHookedAsteroid() { return(hookedAsteroid); }
+
+    /**
+     * Sets what asteroid the player is hooked onto to the input object.
+     * @param hookedAsteroid the asteroid that the player is hooked onto.
+     */
+    public void setHookedAsteroid(Asteroid hookedAsteroid){ this.hookedAsteroid = hookedAsteroid; }
+
+    /**
+     * Returns a string holding the name of the ship.
+     * @return a String holding the name of the ship.
+     */
+    public String getName() { return(name); }
+
+    /**
+     * Sets the name of the ship to the input string.
+     * @param name a string the name of the ship
+     */
+    public void setName(String name){ this.name = name; }
+
+    /**
+     * Return a string holding the name of the player.
+     * @return a string holding the name of the player.
+     */
+    public String getPlayerName(){ return(playerName); }
+
+    /**
+     * Sets the name of the player to the input string.
+     * @param playerName a string holding the name of the player.
+     */
+    public void setPlayerName(String playerName){ this.playerName = playerName; }
+    /*public String toString(){
+        String returnString = "";
+        super.toString()
+    }*/
 }
