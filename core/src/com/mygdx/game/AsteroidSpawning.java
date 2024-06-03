64c0a61cb768c6ef59b7346e4ed1a4b74be988ad
Matthew Witherspoon <92943293+SpoonsDev@users.noreply.github.com>
2024-06-03 13:38:03 -0400

    Most likely finished asteroid spawning class still need check functionality.

--- core/src/com/mygdx/game/AsteroidSpawning.java
+++ core/src/com/mygdx/game/AsteroidSpawning.java
@@ -9,23 +9,36 @@ public class AsteroidSpawning {
         }
         spawnAsteroids(player,asteroids);
         deleteAsteroids(player,asteroids);
+        for(int i = 0; i < objects.size(); i++){
+            objects.remove(0);
+        }
+        objects.add(player);
+        for(int i = 0; i < asteroids.size(); i++){
+            objects.add(asteroids.get(i));
+        }
     }
     private static void spawnAsteroids(Player player, ArrayList<Asteroid> asteroids){
         double posX = player.getPosX();
         double posY = player.getPosY();
         double slope = player.getVelY()/player.getVelX();
+        double multi = 1;
         if(player.getVelX() > 0){
-            for(int i = 0; i < 4; i++){
-
-            }
+            multi = 1;
         }
         else{
-
+            multi = -1;
+        }
+        posX += 625* multi;
+        posY += slope*625*multi;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 84; j++){
+                AsteroidSpawning.spawnAtPos(asteroids,posX,posY,250, 0.5,10,10,5,20);
+            }
         }
     }
     private static void spawnAtPos(ArrayList<Asteroid> asteroids, double posX, double posY, double spread, double maxVel, double minRadius, double maxRadius, double minMass, double maxMass){
-        posX += (Math.random()*spread*2)-1;
-        posY += (Math.random()*spread*2)-1;
+        posX += (Math.random()*spread*2)-spread;
+        posY += (Math.random()*spread*2)-spread;
         double velX = (Math.random()*maxVel*2)-1;
         double velY = (Math.random()*maxVel*2)-1;
         double radius = (Math.random()*(maxRadius - minRadius)) + minRadius;
@@ -38,7 +51,7 @@ public class AsteroidSpawning {
         for(int i = 0; i < asteroids.size(); i++){
             currentAsteroid = asteroids.get(i);
             dist = Math.sqrt(Math.pow(currentAsteroid.getPosX() - player.getPosX(), 2) + Math.pow(currentAsteroid.getPosY() - player.getPosY(), 2));
-            if(dist > 2500){
+            if(dist > 5000){
                 asteroids.remove(i);
             }
         }
