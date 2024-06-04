c2ecca8d09a566fb102ad2814d5d6a0021985954
umarkaahaf <umarkashaf04@gmail.com>
2024-06-04 13:41:34 -0400

    Commented stuff again

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Galaxilize"); // set the title of the game
-		config.setWindowedMode(800, 800); // make the menu canvas of the game
+		config.setWindowedMode(800, 800); // set the title of the game
 		config.useVsync(true);// Enable vertical synchronization to reduce tearing of the screen
 		config.setForegroundFPS(60); // set the foreground to 60 fps
 		new Lwjgl3Application(new Galaxilize(), config);// create a new instance application within your application
