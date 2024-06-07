b67790ca903925197ff49e3d749ff12d9d9ccc0a
Matthew Witherspoon <92943293+SpoonsDev@users.noreply.github.com>
2024-06-07 13:51:07 -0400

    Finished commenting the asteroid spawning algorithm.

--- core/src/com/mygdx/game/AsteroidSpawning.java
+++ core/src/com/mygdx/game/AsteroidSpawning.java
@@ -28,60 +28,78 @@ public class AsteroidSpawning {
         }
     }
 
+    /**
+     * spawns asteroids around the player's position.
+     * @param player the player.
+     * @param asteroids an arraylist of all asteroids currently in the world.
+     */
     private static void spawnAsteroids(Player player, ArrayList<Asteroid> asteroids) {
+        //Create variables for storing basic data about the player.
         double posX = player.getPosX();
         double posY = player.getPosY();
-        double slope = player.getVelY() / player.getVelX();
-        double multi = 1;
-        double dist = 0;
-        Asteroid tempAst;
-        boolean detected = false;
-        if (player.getVelX() > 0) {
-            multi = 1;
-        } else {
-            multi = -1;
-        }
-        for(int i = - 1; i <= 1; i++) {
-            for (int j = 0; j < 5; j++) {
-                spawnAtPos(asteroids, posX - 650, posY + i * 500, 250, 0.5, 5,100);
+        for(int i = - 1; i <= 1; i++) { //Loop through 3 values spawning three clusters of asteroids just outside of each side of the screen.
+            for (int j = 0; j < 5; j++) { //Spawns 5 asteroids on the left side of the screen.
+                spawnAtPos(asteroids, posX - 700, posY + i * 500, 250, 0.5, 5,100);
             }
-            for (int j = 0; j < 5; j++) {
-                spawnAtPos(asteroids, posX + 650, posY + i * 500, 250, 0.5, 5, 100);
+            for (int j = 0; j < 5; j++) { //Spawns 5 asteroids on the right side of the screen.
+                spawnAtPos(asteroids, posX + 700, posY + i * 500, 250, 0.5, 5, 100);
             }
-            for (int j = 0; j < 5; j++) {
-                spawnAtPos(asteroids, posX + i * 500, posY + 650, 250, 0.5, 5, 100);
+            for (int j = 0; j < 5; j++) { //Spawns 5 asteroids on the top side of the screen.
+                spawnAtPos(asteroids, posX + i * 500, posY + 700, 250, 0.5, 5, 100);
             }
-            for (int j = 0; j < 5; j++) {
-                spawnAtPos(asteroids, posX + i * 500, posY - 650, 250, 0.5, 5, 100);
+            for (int j = 0; j < 5; j++) { //Spawns 5 asteroids on the bottom side of the screen.
+                spawnAtPos(asteroids, posX + i * 500, posY - 700, 250, 0.5, 5, 100);
             }
         }
     }
+
+    /**
+     * Spawns a randomly generated asteroid at a position.
+     * @param asteroids the array list of all currently existing asteorids.
+     * @param posX the x position to base the asteroid generation around.
+     * @param posY the y position to base the asteroid generation around.
+     * @param spread the maximum distance that asteroids can be spawned from the point.
+     * @param maxVel the maximum velocity an asteroid can be spawned with.
+     * @param minSize the minimum size an asteroid can be spawned with.
+     * @param maxSize the maximum size an asteroid can be spawned with.
+     */
     private static void spawnAtPos(ArrayList<Asteroid> asteroids, double posX, double posY, double spread, double maxVel, double minSize, double maxSize){
+        //Finds a position to attempt to spawn an asteroid at.
         posX += (Math.random()*spread*2)-spread;
         posY += (Math.random()*spread*2)-spread;
-        boolean spawn = true;
-        for(int i = 0; i < positions.size(); i+= 2){
+        boolean spawn = true; //Sets the variable that determines whether an asteroid will be spawned to true.
+        for(int i = 0; i < positions.size(); i+= 2){ //Loops through every previous position that has spawned an asteroid.
+            //Checks the distance from each point to the current position that the asteroid is attmepting to be spawned at.
             double dist = Math.sqrt(Math.pow(positions.get(i) - posX, 2) + Math.pow(positions.get(i + 1) - posY, 2));
-            if(dist < exclusionRadius.get(i/2)){
+            if(dist < exclusionRadius.get(i/2)){ //Compares the distance between the radius that prevents spawning on the previously spawned positions.
                 spawn = false;
             }
         }
-        if(spawn) {
+        if(spawn) { //Spawns an asteroid if proximity does not impede it.
+            //Add the position that is spawning the asteroid to the array list.
             positions.add(posX);
             positions.add(posY);
+            //Add a random exclusion zone around the asteroid for less uniform spacing between asteroids.
             exclusionRadius.add((int)(Math.random()*200) + 200);
+            //Randomizes attributes of the asteroid.
             double velX = (Math.random() * maxVel * 2) - maxVel;
             double velY = (Math.random() * maxVel * 2) - maxVel;
             double size = (Math.random() * (maxSize - minSize)) + minSize;
             double radius = size/2;
             double mass =  size;
-            asteroids.add(new Asteroid(posX, posY, velX, velY, mass, radius));
+            asteroids.add(new Asteroid(posX, posY, velX, velY, mass, radius)); //Spawn the asteroid by adding it to the array.
         }
     }
+
+    /**
+     * Save system resources by deleting asteroids that are too far from the player.
+     * @param player the player.
+     * @param asteroids the array lists of current existing asteroids.
+     */
     private static void deleteAsteroids(Player player, ArrayList<Asteroid> asteroids){
         double dist;
         Asteroid currentAsteroid;
-        for(int i = 0; i < asteroids.size(); i++){
+        for(int i = 0; i < asteroids.size(); i++){ //Loop through all asteroids and if they are more than 2000 units from the player delete them.
             currentAsteroid = asteroids.get(i);
             dist = Math.sqrt(Math.pow(currentAsteroid.getPosX() - player.getPosX(), 2) + Math.pow(currentAsteroid.getPosY() - player.getPosY(), 2));
             if(dist > 2000){
